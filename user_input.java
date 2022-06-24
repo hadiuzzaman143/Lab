56c6d4868bb2a82d8b5954f5ffb6b6db3c9c1fbc
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-06-24 19:19:20 +0530

    Update user_input.java

--- user_input.java
+++ user_input.java
@@ -3,10 +3,10 @@ public class user_input
 {
     public static void main(String[] args) 
     {
-        Scanner obj = new Scanner(System.in);
+        Scanner obj = new Scanner(System.in); //System.in is a standard input stream 
         String userName;
         System.out.println("Enter name"); 
-        userName = obj.nextLine();   
+        userName = obj.nextLine();  //reads string    
             
         System.out.println("My name is: " + userName);
             
