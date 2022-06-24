184fbe8e58dd8c94a543b270b6d25a96c405012c
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-06-24 12:16:49 +0530

    Update Add_two_number.java

--- Add_two_number.java
+++ Add_two_number.java
@@ -3,7 +3,8 @@ public class Add_two_number
     public static void main(String args[])   
     {  
         int a = 100, b = 50, sum;  
-        sum = a + b;  
+         // add two number
+        sum = a + b; 
         System.out.println("The sum of numbers is: "+sum);  
     }    
 }
