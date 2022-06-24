9f7e83f57d0ac330de2f38cf125c967213ef428c
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-06-24 19:14:36 +0530

    Update Multiplt_two_numbers.java

--- Multiplt_two_numbers.java
+++ Multiplt_two_numbers.java
@@ -2,9 +2,12 @@ class MultiplyTwoNumbers
 {
     public static void main(String[] args)
     {
+         // f is to ensures that numbers are float DATA TYPE
         float a = 1.5f;
         float b = 2.0f;
+         // to store the multiplied value
         float product = a * b;
+        // to print the product
         System.out.println("The product is: " + product);
     }
 }
