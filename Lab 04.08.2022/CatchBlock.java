9c6f79d6a8d0b891a8d487c5d8e4f47600bda760
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-08-04 21:54:07 +0530

    Add files via upload

---
+++ Lab 04.08.2022/CatchBlock.java
@@ -0,0 +1,31 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CatchBlock 
+{    
+	// class
+	 public static void main(String[] args) 
+	 {    
+		// main method
+	        Scanner temp = new Scanner(System.in);    // scanner object
+	        System.out.print("Enter value: ");
+	        
+	        // try block to read user input
+	        try {
+	            int a = temp.nextInt();
+	            int b = temp.nextInt();
+	            System.out.println(a / b);
+	        } 
+	        catch(InputMismatchException ex) 
+			{
+	            // InputMismatchException is printed only
+	            System.out.println(ex.getClass().toString());
+	        }
+	        catch(ArithmeticException ex) 
+			{
+	            // Print exception
+	            System.out.println(ex);
+	        }
+	        temp.close();   // to close scanner object
+	    }
+}
---
+++ Lab 04.08.2022/TaxCalculator.java
@@ -0,0 +1,55 @@
+public class TaxCalculator 
+{
+ public double CalculateTax(String empName,boolean isIndian,double empSal) 
+  { 
+    double taxAmount = 0;
+    try // try block
+    {
+    
+        if(isIndian ==false) //if not Indian throw this
+        {
+          throw new Exception("The Employee should be an Indian citizen tax");
+        }
+        // logic
+        if (empSal>100000)
+        {
+          return taxAmount = empSal *8/100;
+        }
+       if(empSal>=50000 && empSal<100000)
+       {
+        return taxAmount=empSal *6/100;
+       }
+       if(empSal>=30000 && empSal<50000)
+       {
+        return taxAmount=empSal*5/100;
+       }
+       if (empSal>=10000 && empSal<30000)
+       {
+        return taxAmount=empSal *4/100;
+       }
+       else
+       {
+         throw new Exception("The employee does not neet to pay tax");
+       }
+       }
+       catch(Exception e) 
+       {
+        //catch block
+        System.out.println(e);
+       }
+          return taxAmount;      
+ }  
+}
+ class Calculatator
+{
+  public static void main(String[]args)
+  {
+    // main method
+    TaxCalculator t =new TaxCalculator();
+    System.out.println("tax amount is:"+t.CalculateTax("Ron", false, 34000));
+    System.out.println("tax amount is:"+t.CalculateTax("Tim", true, 1000));
+    System.out.println("tax amount is:"+t.CalculateTax("jack", true, 55000));
+    
+  }
+}
+ 
