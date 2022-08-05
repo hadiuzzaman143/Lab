eec5da36105bcd9183c8d2d5cdb0ec2116546d20
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-08-05 14:07:50 +0530

    Update TaxCalculator.java

--- Lab 04.08.2022/TaxCalculator.java
+++ Lab 04.08.2022/TaxCalculator.java
@@ -1,4 +1,4 @@
-public class TaxCalculator 
+public class TaxCalculator //class
 {
  public double CalculateTax(String empName,boolean isIndian,double empSal) 
   { 
@@ -11,6 +11,10 @@ public class TaxCalculator
           throw new Exception("The Employee should be an Indian citizen tax");
         }
         // logic
+        if(empName=="")
+        {
+          throw new Exception("The employee name cannot be empty");
+        }
         if (empSal>100000)
         {
           return taxAmount = empSal *8/100;
@@ -46,9 +50,15 @@ public class TaxCalculator
   {
     // main method
     TaxCalculator t =new TaxCalculator();
+    System.out.println("Ron :");
     System.out.println("tax amount is:"+t.CalculateTax("Ron", false, 34000));
+    System.out.println("Tim :");
     System.out.println("tax amount is:"+t.CalculateTax("Tim", true, 1000));
+    System.out.println("jack :");
     System.out.println("tax amount is:"+t.CalculateTax("jack", true, 55000));
+    System.out.println("null :");
+    System.out.println("tax amount is:"+t.CalculateTax("", true, 30000));
+    
     
   }
 }
