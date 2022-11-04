d8613f7d15bf7121a90034dd59daa6fa462003e6
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-11-04 15:32:08 +0530

    Add files via upload

---
+++ Spring CRUD Lab 03.11.2022/springboot-backend/src/main/java/com/hadiuz/springboot/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.hadiuz.springboot.service;
+
+import java.util.List;
+
+import com.hadiuz.springboot.model.Employee;
+
+public interface EmployeeService {
+	Employee saveEmployee(Employee employee);
+	List<Employee> getAllEmployees();
+	Employee getEmployeeById(long id);
+	Employee updateEmployee(Employee employee, long id);
+	void deleteEmployee(long id);
+}
