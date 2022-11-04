1dca268970df99b4a581129c0eb2fc6d0e1ca68d
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-11-04 15:35:12 +0530

    Delete Spring CRUD Lab 03.11.2022/springboot-backend directory

--- Spring CRUD Lab 03.11.2022/springboot-backend/src/main/java/com/hadiuz/springboot/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hadiuz.springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hadiuz.springboot.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-
-}
